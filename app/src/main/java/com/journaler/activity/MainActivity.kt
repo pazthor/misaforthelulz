dbf9547caa117cce7f576c830801cd7f6114406e
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-08-16 13:39:26 -0500

    Update MainActivity: add onCreate function for the Fragment.

--- app/src/main/java/com/journaler/activity/MainActivity.kt
+++ app/src/main/java/com/journaler/activity/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.journaler.activity
 
+import android.os.Bundle
 import com.journaler.Journaler
 import com.journaler.R
+import com.journaler.fragment.ItemsFragment
 
 class MainActivity : BaseActivity() {
-    override val tag = "Main.Activity"
+    override val tag = "Main Activity"
     override fun getLayout() = R.layout.activity_main
     override fun getActivityTittle() =R.string.app_name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fragment = ItemsFragment()
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+    }
 }
